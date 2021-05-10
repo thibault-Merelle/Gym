6a0e0a07bd54a44383edca7e24d46b88c5d40f83
SIMPLON <tellai>
2021-05-10 17:36:15 +0200

    authentification

--- back-end/controllers/auth.controller.ts
+++ back-end/controllers/auth.controller.ts
@@ -1,7 +1,75 @@
+// const { signUpErrors, signInErrors } = require("../utils/errors.utils");
+import jwt from "jsonwebtoken"
+const maxAge = 3 * 24 * 60 * 60 * 1000; // la variable qui permet de faire expirer une session
+import {MongoClient, ObjectID} from "mongodb";
+const url = process.env.URL_MONGO;
+import bcrypt from "bcrypt"
+const saltRounds = 10;
 
 
-module.exports.signUp = async (req, res) => {}
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+    expiresIn: maxAge,
+  });
+};
 
-module.exports.signIn = async (req, res) => {}
+module.exports.signUp = async (req, res) => {
+  const { pseudo, email, password } = req.body;
+  let passHash;
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      passHash = hash;
+    });
+  });
 
-module.exports.logOut = (req, res) => {}
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      var myobj = {
+        pseudo: pseudo,
+        email: email,
+        password: passHash,
+      };
+      dbo.collection("user").insertOne(myobj, (err, result) => {
+        res.status(201).json({ user: result.insertedId });
+      });
+    });
+  } catch (err) {
+    //   const errors = signUpErrors(err);
+    res.status(200).send(err);
+  }
+};
+
+module.exports.signIn = (req, res) => {
+  const { pseudo, password } = req.body;
+  let id;
+  let pass;
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.collection("user").findOne({ pseudo }, async function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      pass = result.password;
+      console.log(pass);
+      const auth = await bcrypt.compare(password, pass);
+      if (auth) {
+        id = result._id;
+        const token = createToken(id);
+        res.cookie("jwt", token, { httpOnly: true, maxAge });
+        res.send("ok");
+      } else {
+        console.log("Password invalid");
+        res.send("fail");
+      }
+      db.close();
+    });
+  });
+};
+
+module.exports.logOut = (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 }); // pour logOut le user on va juste vider le cookie de son token
+  res.redirect("/");
+};
--- back-end/server.ts
+++ back-end/server.ts
@@ -5,8 +5,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
 const memeRoutes = require("./routes/meme.routes");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 require("dotenv").config({ path: "./config/.env" });
-import { Console } from "console";
 import jwt from "jsonwebtoken"
 
 
@@ -26,75 +26,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+// jwt 
+app.get('*', checkUser); // l'etoile nous permet de selectionner toutes les routes et ainsi de faire la verification sur chaque route
+app.get('/jwtid', requireAuth)
 
 // routes
 app.use("/api/user", userRoutes);
 app.use("/api/meme", memeRoutes);
-
-const users = [
-  { id: 1, name: "Marwa", mail: "marwa@simplon.com", password: "123454321" },
-  { id: 2, name: "Alex", mail: "mskzia@gmail.com", password: "1234RJKDGS" },
-  {
-    id: 3,
-    name: "Max",
-    mail: "mskzifhahfLIa@gmail.com",
-    password: "1234Ry!§JKDGS",
-  },
-  {
-    id: 4,
-    name: "Bron",
-    mail: "mskJHDKUGLDNzia@gmail.com",
-    password: "123JHDIUYACO4RJKDGS",
-  },
-];
-
-const usersMapped = users.map((user)=>{
-  return {id : user.id, name : user.name}
-})
-
-const key_jwt = "shhhhh";
-
-//login
-app.post("/login", (req, res) => {
-  const {mail, password} = req.body
-  for(let i = 0; i < users.length; i++){
-    let user = users[i]
-    if (user.mail == mail) {
-      if (user.password == password){
-        const token = jwt.sign({id : user.id}, key_jwt);
-        res.send(token)
-      }else {
-        res.send("mauvais password")
-      }
-      return
-    }
-  }
-  console.log(mail, password)
-
-  res.send("mauvais user")
-})
-
-const authentification = (req : Request, res : Response, next: NextFunction)=>{
-  try {
-    const token = req.query.token as string;
-    var decoded = jwt.verify(token, key_jwt);
-    console.log("ok")
-    // res.send("token ok")
-    next()
-  }catch(err){
-    console.log(err)
-    res.send("token fail")
-  }
-}
-
-app.get("/profil", authentification,(req, res) =>{
-  console.log("profil")
-  res.send("profil")
-})
-app.post("/privee", authentification, (req, res) =>{
-  console.log("privee")
-  res.send("privee")
-})
 
 
 // server
