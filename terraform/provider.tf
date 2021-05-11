932c1206cfab3ac6eff626d36cff914b985b6982
Thibault_Merelle <merellethibault@gmail.com>
2021-05-11 09:53:00 +0200

    providers terraform cloud

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,27 +1,30 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+    azurerm         = {
+      source        = "hashicorp/azurerm"
+      version       = "=2.46.0"
     }
   }
-    backend "remote" {
--   organization = "REPLACE_ME"
-+   organization = "simplon"
+  backend "remote" {
+    organization    = "simplon"
+    subscription_id = github_actions_secret.gym_subscription_id
+    client_id       = github_actions_secret.gym_client_id
+    client_secret   = github_actions_secret.gym_client_secret
+    tenant_id       = github_actions_secret.gym_tenant_id
 
     workspaces {
-      name = "github-actions-demo"
+      name          = "github-actions-demo"
     }
-}
+  }
 
 
 # Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-    #Add those as env variables
-
-  subscription_id = github_actions_secret.gym_subscription_id
-  client_id       = github_actions_secret.gym_client_id
-  client_secret   = github_actions_secret.gym_client_secret
-  tenant_id       = github_actions_secret.gym_tenant_id
+# provider "azurerm" {
+#   features {}
+#     #Add those as env variables
+#     subscription_id = github_actions_secret.gym_subscription_id
+#     client_id       = github_actions_secret.gym_client_id
+#     client_secret   = github_actions_secret.gym_client_secret
+#     tenant_id       = github_actions_secret.gym_tenant_id
+#   }
 }
