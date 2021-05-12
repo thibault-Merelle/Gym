58b47db2697794ef7caed98a162810920462f9c1
Thibault_Merelle <merellethibault@gmail.com>
2021-05-12 12:01:19 +0200

    secrests handlers to test

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,63 +2,22 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {
+}
+
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
+  # required_providers {
+  #   azurerm = {
+  #     source  = "hashicorp/azurerm"
+  #     version = "=2.46.0"
+  #   }
+  # }
   backend "azurerm" {
     resource_group_name  = "terraform"
     storage_account_name = "gymblobstoragebackend"
     container_name       = "terraformci"
     key                  = "terraform.tfstate"
     
-    access_key           = "Ue+RAvoV9XUBSpm7fAi3NU7O80b0E3mRoHThWHbk3QLzhkiR0L/UPBkVuRFRb2HWMp/2KalAGwxBXydHs2hSEQ=="
+    access_key           = "TyEEmEeTHeS9nEZrZYBir5pbf4OQg08lpSVUf8MAn8y63wphhXl3WDLc0i0+PMDlUxcWz77wiojJNdghnnSMtQ=="
   }
 }
-
-
-
-
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "=2.46.0"
-#     }
-#   }
-# }
-
-
-# # Configure the Microsoft Azure Provider
-# provider "azurerm" {
-#   features {}
-#     #Add those as env variables
-
-#   subscription_id = github_actions_secret.gym_subscription_id
-#   client_id       = github_actions_secret.gym_client_id
-#   client_secret   = github_actions_secret.gym_client_secret
-#   tenant_id       = github_actions_secret.gym_tenant_id
-# }
-
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#     }
-#     random = {
-#       source = "hashicorp/random"
-#     }
-#   }
-
-#   backend "remote" {
-# -   organization = "REPLACE_ME"
-# +   organization = "YOUR_ORGANIZATION_NAME"
-
-#     workspaces {
-#       name = "gh-actions-demo"
-#     }
-#   }
-# }
